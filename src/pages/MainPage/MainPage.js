577dbb810a79ed262693f8ccb08177a8bbc691ac
yeonjeonglee <yeonjeong.lee00@gmail.com>
2021-01-08 00:05:37 +0900

    [#18] markup - 상품컴포넌트 마크업
    - 하나의 상품컴포넌트 완성
    - 메인페이지 상품 리스트 줄 완성
    - css 추가
    - 변수 이름 변경
    - 사용하지 않는 boostrap 지우기
    - 코드 prettier 적용

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -4,11 +4,19 @@ import classNames from 'classnames/bind';
 
 /* Internal dependencies */
 import styles from './MainPage.module.scss';
+import GoodsComponent from 'components/MainPage/GoodsComponent'
 
 const cx = classNames.bind(styles);
 
 function MainPage() {
-  return <div>메인페이지입니다.</div>;
+  return <div>
+    <div className = {cx('list1')}>
+     <GoodsComponent />
+     <GoodsComponent />
+     <GoodsComponent />
+    </div>
+  </div>;
+  
 }
 
 export default MainPage;
