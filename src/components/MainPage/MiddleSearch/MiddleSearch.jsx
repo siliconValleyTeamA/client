71fe2ad455bcb2abda8279f57ed4047fbb7a85fe
OMEGA-Y <science_co@naver.com>
2021-01-09 22:57:34 +0900

    [#22] markup - 메인 페이지 중간 전체보기 bar 마크업 - search bar 컴포넌트 - selection bar 컴포넌트

---
+++ src/components/MainPage/MiddleSearch/MiddleSearch.jsx
@@ -0,0 +1,81 @@
+/* External dependencies */
+import React, { createContext, useState, memo, useContext } from 'react';
+import classNames from 'classnames/bind';
+import { FiSearch } from 'react-icons/fi';
+
+/* Internal dependencies */
+import styles from './MiddleSearch.module.scss';
+const cx = classNames.bind(styles);
+
+function MiddleSearch() {
+  return (
+    <div className={cx('middle-navigation')}>
+      <div className={cx('header')}>
+        <h1> 전체보기 </h1>
+      </div>
+      <div className={cx('navigation')}>
+        <SearchBar />
+        <div className={cx('filter')}>
+          <FilterProvider>
+            <FilterContent />
+          </FilterProvider>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function SearchBar(props) {
+  return (
+    <div className={cx('search')}>
+      <input
+        type="text"
+        placeholder="검색"
+        onChange={event => {}}
+        class={cx('search-input')}
+      />
+      <button onClick={() => {}}>
+        <FiSearch />
+      </button>
+    </div>
+  );
+}
+
+const FilterContent = memo(() => {
+  return (
+    <div>
+      <FilterPicker />
+    </div>
+  );
+});
+
+const FilterContext = createContext();
+
+function FilterProvider({ children }) {
+  const [menu, setMenu] = useState('P');
+
+  return (
+    <FilterContext.Provider value={{ menu, setMenu }}>
+      {children}
+    </FilterContext.Provider>
+  );
+}
+
+function FilterPicker() {
+  const { menu, setMenu } = useContext(FilterContext);
+
+  return (
+    <select
+      value={menu}
+      onChange={event => {
+        setMenu(event.target.value);
+      }}
+    >
+      <option value="P">펀딩률순</option>
+      <option value="M">펀딩금액순</option>
+      <option value="T">마감임박순</option>
+    </select>
+  );
+}
+
+export default MiddleSearch;
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -4,19 +4,23 @@ import classNames from 'classnames/bind';
 
 /* Internal dependencies */
 import styles from './MainPage.module.scss';
-import GoodsComponent from 'components/MainPage/GoodsComponent'
+
+import GoodsComponent from 'components/MainPage/GoodsComponent';
+import MiddleSearch from 'components/MainPage/MiddleSearch';
 
 const cx = classNames.bind(styles);
 
 function MainPage() {
-  return <div>
-    <div className = {cx('list1')}>
-     <GoodsComponent />
-     <GoodsComponent />
-     <GoodsComponent />
+  return (
+    <div>
+      <MiddleSearch />
+      <div className={cx('list1')}>
+        <GoodsComponent />
+        <GoodsComponent />
+        <GoodsComponent />
+      </div>
     </div>
-  </div>;
-  
+  );
 }
 
 export default MainPage;
