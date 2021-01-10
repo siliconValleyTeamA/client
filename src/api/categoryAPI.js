3b2502473de8b9edc7c049519e893035b15df085
nohgijin <ynda11111@naver.com>
2021-01-11 03:15:11 +0900

    [#33] feature - 카테고리 mock data 생성 - CategoryList 생성, 여기서 Category 호출

---
+++ src/api/categoryAPI.js
@@ -0,0 +1,24 @@
+const mockCategoriesList = {
+  data: [
+    {
+      id: 1,
+      text: '전체보기',
+      img: '',
+      link: '',
+    },
+    {
+      id: 2,
+      text: '테크-가전',
+      img: '',
+      link: '',
+    },
+    {
+      id: 3,
+      text: '패션-잡화',
+      img: '',
+      link: '',
+    },
+  ],
+};
+
+export default mockCategoriesList;
