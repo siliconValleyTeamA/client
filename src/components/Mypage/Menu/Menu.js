dcc39605760d9e9c65d0528a6cb4a5aa601417b6
dbckdgjs369 <dbckdgjs1130@naver.com>
2021-01-11 18:31:48 +0900

    [#37] markup- 국가 설정 및 잔여포인트 페이지

--- src/components/Mypage/Menu/Menu.js
+++ src/components/Mypage/Menu/Menu.js
@@ -1,6 +1,7 @@
 /* External dependencies */
 import React from 'react';
 import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
 import { AiFillHeart } from 'react-icons/ai';
 import { RiArrowRightSLine } from 'react-icons/ri';
 import { RiShoppingBag3Fill } from 'react-icons/ri';
@@ -29,11 +30,13 @@ function Menu() {
           <RiArrowRightSLine className={cx('RiArrowRightSLine')} />
         </li>
       </Link>
-      <li className={cx('country')}>
-        <BiWorld className={cx('BiWorld')} />
-        <div className={cx('title')}>개인정보 설정</div>
-        <RiArrowRightSLine className={cx('RiArrowRightSLine')} />
-      </li>
+      <Link to="/setcountry">
+        <li className={cx('country')}>
+          <BiWorld className={cx('BiWorld')} />
+          <div className={cx('title')}>개인정보 설정</div>
+          <RiArrowRightSLine className={cx('RiArrowRightSLine')} />
+        </li>
+      </Link>
     </ol>
   );
 }
