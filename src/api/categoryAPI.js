d7b7267a5f601a298855076c2ef5074d7591cbec
nohgijin <ynda11111@naver.com>
2021-01-11 22:24:08 +0900

    [#43] feat - category 왼쪽 오른쪽 상태관리

--- src/api/categoryAPI.js
+++ src/api/categoryAPI.js
@@ -18,7 +18,79 @@ const mockCategoriesList = {
       img: '',
       link: 'fashion',
     },
-    
+    {
+      id: 4,
+      category: '패션-잡화',
+      img: '',
+      link: 'fashion',
+    },
+    {
+      id: 5,
+      category: '패션-잡화',
+      img: '',
+      link: 'fashion',
+    },
+    {
+      id: 6,
+      category: '패션-잡화',
+      img: '',
+      link: 'fashion',
+    },
+    {
+      id: 7,
+      category: '패션-잡화',
+      img: '',
+      link: 'fashion',
+    },
+    {
+      id: 8,
+      category: '패션-잡화',
+      img: '',
+      link: 'fashion',
+    },
+    {
+      id: 9,
+      category: '패션-잡화',
+      img: '',
+      link: 'fashion',
+    },
+    {
+      id: 10,
+      category: '패션-잡화',
+      img: '',
+      link: 'fashion',
+    },
+    {
+      id: 11,
+      category: '패션-잡화',
+      img: '',
+      link: 'fashion',
+    },
+    {
+      id: 12,
+      category: '패션-잡화',
+      img: '',
+      link: 'fashion',
+    },
+    {
+      id: 13,
+      category: '패션-잡화',
+      img: '',
+      link: 'fashion',
+    },
+    {
+      id: 14,
+      category: '패션-잡화',
+      img: '',
+      link: 'fashion',
+    },
+    {
+      id: 15,
+      category: '패션-잡화',
+      img: '',
+      link: 'fashion',
+    },
+
   ],
 };
 
