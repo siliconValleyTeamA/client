9c1d2689f6be212797a39ae43dd181aa7d8ebfa8
yeonjeonglee <yeonjeong.lee00@gmail.com>
2021-01-11 11:00:05 +0900

    [#11] feature - validation 추가
    - 이메일, 비밀번호 validation 추가

--- src/pages/EmailSignupPage/EmailSignupPage.js
+++ src/pages/EmailSignupPage/EmailSignupPage.js
@@ -1,5 +1,6 @@
 /* External dependencies */
 import React from 'react';
+import { useState } from 'react';
 import classNames from 'classnames/bind';
 import { IoPersonOutline } from 'react-icons/io5';
 import { AiOutlineLock } from 'react-icons/ai';
@@ -9,18 +10,19 @@ import styles from './EmailSignupPage.module.scss';
 
 const cx = classNames.bind(styles);
 const country = [
-  { lang: '영어' },
-  { lang: '한국어' },
-  { lang: '중국어' },
-  { lang: '일본어' },
+  { id: 1, lang: '한국어' },
+  { id: 2, lang: '영어' },
+  { id: 3, lang: '중국어' },
+  { id: 4, lang: '일본어' },
 ];
 
-function InputInfo({ className, type, placeholder }) {
+function InputInfo({ className, type, placeholder, onChange }) {
   return (
     <input
       className={cx({ className })}
       type={type}
       placeholder={placeholder}
+      onChange={onChange}
     />
   );
 }
@@ -40,13 +42,59 @@ function LanguageSelect({ lang }) {
 function InterestSelect({ interest }) {
   return (
     <div className={cx('interest-element')}>
-      <input type="checkbox" />
+      <input type="checkbox" required />
       <p>{interest}</p>
     </div>
   );
 }
 
 function EmailSignupPage() {
+  const [password, setPassword] = useState('✓ 비밀번호를 입력해주세요.');
+  const [email, setEmail] = useState('✓ 이메일을 입력해주세요.');
+  const [name, setName] = useState('✓ 이름을 입력해주세요.');
+
+  const checkName = e => {
+    if (e.target.value.length > 0) {
+      setName('');
+    } else {
+      setName('✓ 이름을 입력해주세요.');
+    }
+  };
+
+  const checkEmail = e => {
+    const regExp = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+    if (e.target.value.length > 0 && !regExp.test(e.target.value)) {
+      setEmail('✓ 이메일을 확인해주세요.');
+    } else if (e.target.value.length === 0) {
+      setEmail('✓ 이메일을 입력해주세요.');
+    } else if (e.target.value.length > 0 && regExp.test(e.target.value)) {
+      setEmail('');
+    }
+  };
+  const checkInputPassword = e => {
+    const regExp = /^[a-zA-Z0-9!@#$%^&*+=-]{8,20}$/i;
+    if (e.target.value.length > 0 && !regExp.test(e.target.value)) {
+      setPassword('✓ 비밀번호를 확인해주세요.');
+    } else if (e.target.value.length === 0) {
+      setPassword('✓ 비밀번호를 입력해주세요.');
+    } else if (e.target.value.length > 0 && regExp.test(e.target.value)) {
+      setPassword('✓ 확인 비밀번호를 입력해주세요.');
+    }
+  };
+
+  const checkPassword = e => {
+    const inputPassword =
+      e.target.parentNode.parentNode.parentNode.childNodes[0].childNodes[0]
+        .value;
+    if (e.target.value.length === 0) {
+      setPassword('✓ 비밀번호를 입력해주세요.');
+    } else if (e.target.value !== inputPassword) {
+      setPassword('✓ 비밀번호가 다릅니다.');
+    } else if (e.target.value === inputPassword) {
+      setPassword('');
+    }
+  };
+
   return (
     <div className={cx('signup')}>
       <h1 className={cx('title')}>회원가입</h1>
@@ -57,25 +105,32 @@ function EmailSignupPage() {
             className={cx('email-input')}
             type="email"
             placeholder="이메일 계정"
+            onChange={checkEmail}
           />
-          <button className={cx('email-auth')} onClick={() => {}}>
-            확인하기
-          </button>
         </div>
-        <Notice text="이메일을 입력해주세요." />
+        <Notice text={email} />
       </div>
       <div className={cx('name')}>
-        <InputInfo className="name-input" type="text" placeholder="이름" />
+        <InputInfo
+          className="name-input"
+          type="text"
+          placeholder="이름"
+          onChange={checkName}
+        />
         <p className={cx('emoji')}>
-          {' '}
           <IoPersonOutline />
         </p>
       </div>
+      <Notice text={name} />
       <div className={cx('password')}>
         <div className={cx('password-input')}>
-          <InputInfo className="" type="password" placeholder="비밀번호 입력" />
+          <InputInfo
+            className=""
+            type="password"
+            placeholder="비밀번호 입력해주세요"
+            onChange={checkInputPassword}
+          />
           <p className={cx('emoji')}>
-            {' '}
             <AiOutlineLock />
           </p>
         </div>
@@ -85,25 +140,22 @@ function EmailSignupPage() {
               className=""
               type="password"
               placeholder="비밀번호 확인"
+              onChange={checkPassword}
             />
             <p className={cx('emoji')}>
-              {' '}
               <AiOutlineLock />
             </p>
           </div>
-          <Notice text="영문, 숫자, 특수문자 (!@#$%^&*+=-)를 조합한 8자 이상" />
+          <Notice text="✓ 영문, 숫자, 특수문자 (!@#$%^&*+=-)를 조합한 8자 이상" />
+          <Notice text={password} />
         </div>
-      </div>
-      <div className={cx('agree')}>
-        <p>전체동의</p>
-        <input className={cx('agree-chkbtn')} type="checkbox" />
       </div>
       <div className={cx('country')}>
         <p>국가선택</p>
         <div>
           <select className={cx('country-list')}>
-            {country.map(language => (
-              <LanguageSelect lang={language.lang} />
+            {country.map((language, index) => (
+              <LanguageSelect key={index} lang={language.lang} />
             ))}
           </select>
         </div>
@@ -123,11 +175,7 @@ function EmailSignupPage() {
           </div>
         </div>
       </div>
-      <div className={cx('agree')}>
-        <p>전체동의</p>
-        <input className={cx('agree-chkbtn')} type="checkbox" />
-      </div>
-      <button className={cx('submit')} type="button">
+      <button className={cx('submit')} type="submit">
         완료
       </button>
     </div>
