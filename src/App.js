bf8a904750549bea8b4b28e3dfd56908e6a5ff4a
OMEGA-Y <science_co@naver.com>
2021-01-11 16:40:00 +0900

    [#36] feature - MyFundingPage, MyJjimPage 생성 및 Router 연결 -mypage에서 funding내역 및 찜 내역으로 링크 연결

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import DetailPage from 'pages/DetailPage';
 import CategoryPage from 'pages/CategoryPage';
 import PopularPage from 'pages/PopularPage';
 import MyPage from 'pages/MyPage';
+import MyFundingPage from 'pages/MyFundingPage';
+import MyJjimPage from 'pages/MyJjimPage';
 import GlobalStyle from 'styles/global-styles';
 
 const MainLayout = styled.div`
@@ -25,6 +27,8 @@ function App() {
           <Route path="/category/:category" exact component={CategoryPage} />
           <Route path="/popular" exact component={PopularPage} />
           <Route path="/detail" component={DetailPage} />
+          <Route path="/mypage/funding" component={MyFundingPage} />
+          <Route path="/mypage/jjim" component={MyJjimPage} />
           <Route path="/mypage" component={MyPage} />
         </Switch>
         <GlobalStyle />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 export { default as DetailPage } from './DetailPage';
 export { default as LoginPage } from './Legacy/LoginPage';
-export { default as MainPage } from './MainPage';
 export { default as SignupPage } from './Legacy/SignupPage';
 export { default as FindIdPage } from './Legacy/FindIdPage';
 export { default as FindPwdPage } from './Legacy/FindPwdPage';
-export { default as PrivacyPage } from './PrivacyPage';
-export { default as EmailSignupPage } from './Legacy/EmailSignupPage';
+export { default as MyFundingPage } from './MyFundingPage';
+export { default as MyJjimPage } from './MyJjimPage';
