1b2918ec6a06239f713c07f53e5931f3452f9cde
yeonjeonglee <yeonjeong.lee00@gmail.com>
2021-01-11 22:51:30 +0900

    [#10] fix - react Hook 사용하게 변경 - class에서 componentDidUpdate를 useEffect Hook을 사용해서 변경 - documnet.location.href를 사용해서 link를 가져오는 것 대신 useLocation을 사용해서 링크를 가져온다. - getElementbyId로 구현한 것을 useRef를 사용해서 변경

--- src/components/global/NavigationBar/NavigationBar.js
+++ src/components/global/NavigationBar/NavigationBar.js
@@ -1,60 +1,64 @@
 /* External dependencies */
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import classNames from 'classnames/bind';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 /* Internal dependencies */
 import styles from './NavigationBar.module.scss';
 
 const cx = classNames.bind(styles);
 
-const nav = [
-  { id: '1', name: '홈', link: '/main' },
-  { id: '2', name: '카테고리', link: '/main' },
-  { id: '3', name: '인기펀딩', link: '/main' },
-  { id: '4', name: '마이페이지', link: '/mypage' },
-];
-
-const changeStyle = id => {
-  document.getElementById(id).style.color = 'white';
-  document.getElementById(id).style.backgroundColor = '#00c4c4';
+const changeStyle = name => {
+  name.current.style.backgroundColor = '#00c4c4';
+  name.current.style.color = 'white';
 };
 
-const styleSetting = () => {
-  let link = document.location.href.split('/')[3];
-  if (link === 'main') {
-    changeStyle('1');
-  } else if (link === 'mypage') {
-    changeStyle('4');
-  }
-};
+function NavigationBar() {
+  const location = useLocation();
+  const homeRef = useRef(1);
+  const categoryRef = useRef(2);
+  const popularPundingRef = useRef(3);
+  const mypageRef = useRef(4);
+  const styled = {
+    background: 'white',
+    color: '#00c4c4',
+  };
 
-class NavigationBar extends React.Component {
-  componentDidMount() {
-    styleSetting();
-  }
-  render() {
-    return (
-      <div className={cx('Header')}>
-        <ul className={cx('navigation_bar')}>
-          {nav.map(menu => (
-            <Link to={menu.link} key={menu.id}>
-              <li
-                key={menu.id}
-                id={menu.id}
-                className={cx('nav')}
-                onClick={e => {
-                  console.log(e.target.id);
-                }}
-              >
-                {menu.name}
-              </li>
-            </Link>
-          ))}
-        </ul>
-      </div>
-    );
-  }
+  const nav = [
+    { id: '1', name: '홈', link: '/main', ref: homeRef },
+    { id: '2', name: '카테고리', link: '/main', ref: categoryRef },
+    { id: '3', name: '인기펀딩', link: '/main', ref: popularPundingRef },
+    { id: '4', name: '마이페이지', link: '/mypage', ref: mypageRef },
+  ];
+
+  useEffect(() => {
+    const link = location.pathname;
+    if (link === '/main') {
+      changeStyle(homeRef);
+    } else if (link === '/mypage') {
+      changeStyle(mypageRef);
+    }
+  });
+
+  return (
+    <div className={cx('Header')}>
+      <ul className={cx('navigation_bar')}>
+        {nav.map(menu => (
+          <Link to={menu.link} key={menu.id}>
+            <li
+              ref={menu.ref}
+              key={menu.id}
+              id={menu.id}
+              className={cx('nav')}
+              style={styled}
+            >
+              {menu.name}
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default NavigationBar;
