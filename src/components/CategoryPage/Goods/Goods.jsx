a81987139f2c0ed132419ddc75401ec92631c046
nohgijin <ynda11111@naver.com>
2021-01-11 14:20:54 +0900

    [#38] chore - a 대신 link 사용

--- src/components/CategoryPage/Goods/Goods.jsx
+++ src/components/CategoryPage/Goods/Goods.jsx
@@ -1,5 +1,6 @@
 /* External dependencies */
 import React from 'react';
+import { Link } from 'react-router-dom';
 import classNames from 'classnames/bind';
 
 /* Internal dependencies */
@@ -11,41 +12,39 @@ function Goods({ data }) {
   let width = data.percent > 100 ? 100 : data.percent;
 
   return (
-    <button>
-      <a href="/detail" onClick={() => {}}>
-        <div className={cx('goods')}>
-          <div className={cx('goods-img')}>
-            <img
-              src="https://img-cf.kurly.com/shop/data/goods/1589438976172z0.jpg"
-              alt="goods-detail"
-            />
-          </div>
-          <div className={cx('goods-title')}>
-            <h3> {data.title} </h3>
-          </div>
-          <div className={cx('goods-catecory-company')}>
-            <span> {data.category} </span>
-            <span> | </span>
-            <span> {data.company}</span>
+    <Link to="/detail">
+      <div className={cx('goods')}>
+        <div className={cx('goods-img')}>
+          <img
+            src="https://img-cf.kurly.com/shop/data/goods/1589438976172z0.jpg"
+            alt="goods-detail"
+          />
+        </div>
+        <div className={cx('goods-title')}>
+          <h3> {data.title} </h3>
+        </div>
+        <div className={cx('goods-catecory-company')}>
+          <span> {data.category} </span>
+          <span> | </span>
+          <span> {data.company}</span>
+        </div>
+        <div>
+          <div
+            className={cx('goods-progressbar')}
+            style={{ width: `${width}%` }}
+          />
+        </div>
+        <div className={cx('goods-progressinfo')}>
+          <div className={cx('goods-progressinfo-percent')}>
+            <p>{data.percent}%</p>
+            <span>・ {data.amount.toLocaleString()}원</span>
           </div>
           <div>
-            <div
-              className={cx('goods-progressbar')}
-              style={{ width: `${width}%` }}
-            />
-          </div>
-          <div className={cx('goods-progressinfo')}>
-            <div className={cx('goods-progressinfo-percent')}>
-              <p>{data.percent}%</p>
-              <span>・ {data.amount.toLocaleString()}원</span>
-            </div>
-            <div>
-              <span>{data.dueDate}일 남음</span>
-            </div>
+            <span>{data.dueDate}일 남음</span>
           </div>
         </div>
-      </a>
-    </button>
+      </div>
+    </Link>
   );
 }
 
