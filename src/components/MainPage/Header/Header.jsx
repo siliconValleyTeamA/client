57cd7d56d13a4a8d9ba3d96248738320dfd356c8
nohgijin <ynda11111@naver.com>
2021-01-11 11:08:23 +0900

    [#33] feature - category redux 연결

---
+++ src/components/MainPage/Header/Header.jsx
@@ -0,0 +1,16 @@
+/* External dependencies */
+import React from 'react';
+import classNames from 'classnames/bind';
+import { useSelector } from 'react-redux';
+
+/* Internal dependencies */
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
+
+function Header() {
+  const category = useSelector(state => state.categoryReducer);
+  return <div className={cx('header')}>{category}</div>;
+}
+
+export default Header;
