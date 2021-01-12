dae8a2d7137c980ca72e1f907731ef08b43cae63
nohgijin <ynda11111@naver.com>
2021-01-12 17:40:16 +0900

    [#56] feature - 찜, 히스토리 목데이터 수정, 시간순으로 정렬

--- src/components/MyDetailPage/GoodsList/GoodsList.jsx
+++ src/components/MyDetailPage/GoodsList/GoodsList.jsx
@@ -5,20 +5,39 @@ import classNames from 'classnames/bind';
 /* Internal dependencies */
 import styles from './GoodsList.module.scss';
 import Goods from 'components/MyDetailPage/Goods';
-import mockUserList from 'api/userAPI';
+import mockJjimList from 'api/jjimAPI';
+import mocHistoryList from 'api/historyAPI';
 
 const cx = classNames.bind(styles);
+
+function sortByJjimTime(a, b) {
+  return a.jjim_date.getTime() > b.jjim_date.getTime()
+    ? -1
+    : a.jjim_date.getTime() < b.jjim_date.getTime()
+    ? 1
+    : 0;
+}
+
+function sortByHistoryTime(a, b) {
+  return a.history_date.getTime() > b.history_date.getTime()
+    ? -1
+    : a.history_date.getTime() < b.history_date.getTime()
+    ? 1
+    : 0;
+}
 
 function GoodsList(props) {
   return (
     <div className={cx('goods-list')}>
       {props.show === 'funding'
-        ? mockUserList.data
-            .filter(data => data.isSponsored === true)
-            .map(goodsData => <Goods key={goodsData.id} data={goodsData} />)
-        : mockUserList.data
-            .filter(data => data.isJjimed === true)
-            .map(goodsData => <Goods key={goodsData.id} data={goodsData} />)}
+        ? mocHistoryList.data
+            .sort(sortByHistoryTime)
+            .map(historyData => (
+              <Goods key={historyData.id} data={historyData} />
+            ))
+        : mockJjimList.data
+            .sort(sortByJjimTime)
+            .map(jjimData => <Goods key={jjimData.id} data={jjimData} />)}
     </div>
   );
 }
