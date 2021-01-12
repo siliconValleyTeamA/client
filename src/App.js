d9749c402819e384f7209b1a9d4f0dd23437c6e4
nohgijin <ynda11111@naver.com>
2021-01-12 17:54:37 +0900

    [#56] chore - funding -> history로 수정

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import DetailPage from 'pages/DetailPage';
 import CategoryPage from 'pages/CategoryPage';
 import PopularPage from 'pages/PopularPage';
 import MyPage from 'pages/MyPage';
-import MyFundingPage from 'pages/MyFundingPage';
+import MyHistoryPage from 'pages/MyHistoryPage';
 import MyJjimPage from 'pages/MyJjimPage';
 import GlobalStyle from 'styles/global-styles';
 import SetProfilePage from 'pages/SetProfilePage/SetProfilePage';
@@ -28,10 +28,9 @@ function App() {
           <Route path="/category/:categoryid" component={CategoryPage} />
           <Route path="/popular" component={PopularPage} />
           <Route path="/detail" component={DetailPage} />
-          <Route path="/mypage/funding" component={MyFundingPage} />
-          <Route path="/mypage/jjim" component={MyJjimPage} />         
-          <Route path="/mypage/setprofile" component={SetProfilePage} />
-          <Route path="/mypage" component={MyPage} />          
+          <Route path="/mypage/history" component={MyHistoryPage} />
+          <Route path="/mypage/jjim" component={MyJjimPage} />
+          <Route path="/mypage" component={MyPage} />
         </Switch>
         <GlobalStyle />
       </MainLayout>
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,5 +3,5 @@ export { default as LoginPage } from './Legacy/LoginPage';
 export { default as SignupPage } from './Legacy/SignupPage';
 export { default as FindIdPage } from './Legacy/FindIdPage';
 export { default as FindPwdPage } from './Legacy/FindPwdPage';
-export { default as MyFundingPage } from './MyFundingPage';
+export { default as MyHistoryPage } from './MyHistoryPage';
 export { default as MyJjimPage } from './MyJjimPage';
