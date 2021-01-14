fdcc4f3948626cf118a83163c540cd568d033b3d
nohgijin <ynda11111@naver.com>
2021-01-14 23:30:34 +0900

    [#72] feat - 카트 추가, 찜 삭제 API 작성

--- src/api/jjimAPI.js
+++ src/api/jjimAPI.js
@@ -1,46 +1,5 @@
-const mockJjimList = {
-  data: [
-    {
-      id: 1,
-      title: '[비블리X위글위글] 선빼고 다드려요! 2만원대 무선 미니 고데기',
-      percent: 1234,
-      amount: 2131232,
-      dueDate: 23,
-      jjim_date: new Date(2020, 10, 1),
-    },
-    {
-      id: 2,
-      title: '[비블리X위글위글] 선빼고 다드려요! 2만원대 무선 미니 고데기',
-      percent: 1234,
-      amount: 2131232,
-      dueDate: 23,
-      jjim_date: new Date(2020, 10, 2),
-    },
-    {
-      id: 3,
-      title: '[비블리X위글위글] 선빼고 다드려요! 2만원대 무선 미니 고데기',
-      percent: 1234,
-      amount: 2131232,
-      dueDate: 23,
-      jjim_date: new Date(2020, 10, 3),
-    },
-    {
-      id: 4,
-      title: '[비블리X위글위글] 선빼고 다드려요! 2만원대 무선 미니 고데기',
-      percent: 1234,
-      amount: 2131232,
-      dueDate: 23,
-      jjim_date: new Date(2020, 10, 4),
-    },
-    {
-      id: 5,
-      title: '[비블리X위글위글] 선빼고 다드려요! 2만원대 무선 미니 고데기',
-      percent: 1234,
-      amount: 2131232,
-      dueDate: 23,
-      jjim_date: new Date(2020, 10, 5),
-    },
-  ],
-};
+import axios from './axios';
 
-export default mockJjimList;
+export const removeJjimAPI = ({ projectId }) => {
+  return axios.delete(`/api/user/jjim`, { data: { projectId } });
+};
