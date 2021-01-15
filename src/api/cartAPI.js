be59c5cb565eb448d3d756b887b5197b40e2a215
OMEGA-Y <science_co@naver.com>
2021-01-15 12:24:16 +0900

    [#74] feature - createCart, removeCart, modifyCart, getProjectListInCart

--- src/api/cartAPI.js
+++ src/api/cartAPI.js
@@ -1,5 +1,17 @@
 import axios from './axios';
 
 export const createCartAPI = ({ projectId }) => {
-  return axios.post(`/api/user/cart`, { projectId });
+  return axios.post(`/api/user/carts`, { projectId });
+};
+
+export const modifyCartAPI = ({ projectId, point }) => {
+  return axios.put('api/user/carts', { data: { projectId, point } });
+};
+
+export const removeCartAPI = ({ projectId }) => {
+  return axios.delete(`/api/user/carts`, { data: { projectId } });
+};
+
+export const getProjectListInCartAPI = () => {
+  return axios.get(`/api/user/carts`);
 };
