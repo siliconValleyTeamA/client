84ecfb59dd60260ccd3982014d47729380b6a479
OMEGA-Y <science_co@naver.com>
2021-01-15 12:24:16 +0900

    [#74] feature - createCart, removeCart, modifyCart, getProjectListInCart

--- src/components/DetailPage/Drawer/Drawer.jsx
+++ src/components/DetailPage/Drawer/Drawer.jsx
@@ -1,14 +1,26 @@
 /* External dependencies */
 import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import classNames from 'classnames/bind';
 import { AiOutlineMinus } from 'react-icons/ai';
+import { createCartAPI } from 'api/cartAPI';
 
 /* Internal dependencies */
 import styles from './Drawer.module.scss';
 const cx = classNames.bind(styles);
 
-function Drawer({ open, setOpen, data}) {
+function Drawer({ open, setOpen, data }) {
+  const link = useLocation();
+  const projectId = link.pathname.split('/')[2];
+
   const [money, setMoney] = useState('');
+
+  const createCart = () => {
+    setOpen(false);
+    createCartAPI(projectId).then(result => {
+      console.log(result.data);
+    });
+  };
 
   return (
     <div className={cx('drawer')}>
@@ -24,7 +36,7 @@ function Drawer({ open, setOpen, data}) {
           <div className={cx('product-details-cart')}>
             <img
               className={cx('product-details-cart-image')}
-              src= {data.img}
+              src={data.img}
               alt="장바구니용 사진"
             />
             <div className={cx('product-details-cart-buttons')}>
@@ -43,7 +55,10 @@ function Drawer({ open, setOpen, data}) {
                   );
                 }}
               />
-              <div onClick = { ()=>{setOpen(false)}} className={cx('product-details-cart-buttons-confirm')}>
+              <div
+                onClick={createCart}
+                className={cx('product-details-cart-buttons-confirm')}
+              >
                 펀딩하기
               </div>
             </div>
--- src/components/MyDetailPage/CartPage/CartList/CartList.jsx
+++ src/components/MyDetailPage/CartPage/CartList/CartList.jsx
@@ -1,27 +1,28 @@
 /* External dependencies */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import classNames from 'classnames/bind';
 
 /* Internal dependencies */
 import styles from './CartList.module.scss';
 import CartGoods from 'components/MyDetailPage/CartPage/CartGoods';
-import mockCartList from 'api/cartAPI';
+import { getProjectListInCartAPI } from 'api/cartAPI';
 
 const cx = classNames.bind(styles);
 
-function sortByCartTime(a, b) {
-  return a.cart_date.getTime() > b.cart_date.getTime()
-    ? -1
-    : a.cart_date.getTime() < b.cart_date.getTime()
-    ? 1
-    : 0;
-}
 function CartList() {
+  const [projectList, setProjectList] = useState([]);
+
+  useEffect(() => {
+    getProjectListInCartAPI().then(result => {
+      setProjectList(result.data);
+    });
+  }, []);
+
   return (
     <div className={cx('cart-list')}>
-      {/* {mockCartList.data.sort(sortByCartTime).map(cartData => (
+      {projectList.map(cartData => (
         <CartGoods key={cartData.id} data={cartData} />
-      ))} */}
+      ))}
     </div>
   );
 }
