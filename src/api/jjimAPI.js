25ae1b001cc6fe91e68667910d6e66681724734d
yeonjeonglee <yeonjeong.lee00@gmail.com>
2021-01-15 15:54:37 +0900

    [#73] feat - jjim, logout, login 백엔드 연결 - login 아직 구현 못함

--- src/api/jjimAPI.js
+++ src/api/jjimAPI.js
@@ -3,3 +3,13 @@ import axios from './axios';
 export const removeJjimAPI = ({ projectId }) => {
   return axios.delete(`/api/user/jjim`, { data: { projectId } });
 };
+
+export const getJjimListAPI = () => {
+  return axios.get(`/api/user/jjim`);
+};
+
+export const createJjimAPI = projectId => {
+  return axios.post(`/api/user/jjim`, {
+    projectId: projectId,
+  });
+};
--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -7,3 +7,11 @@ export const getPointAPI = () => {
 export const getLanguageAPI = () => {
   return axios.get(`/api/user/language`);
 };
+
+export const logOutAPI = () => {
+  return axios.post('api/user/logout');
+};
+
+export const logInAPI = () => {
+  return axios.post('api/user/login');
+};
