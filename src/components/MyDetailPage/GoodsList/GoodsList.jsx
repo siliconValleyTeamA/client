c966287f50f795dfb15a1dc26557695c41fedb74
nohgijin <ynda11111@naver.com>
2021-01-16 04:25:36 +0900

    [#87] fix - history api 구현

--- src/components/MyDetailPage/GoodsList/GoodsList.jsx
+++ src/components/MyDetailPage/GoodsList/GoodsList.jsx
@@ -1,54 +1,36 @@
 /* External dependencies */
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import classNames from 'classnames/bind';
 
 /* Internal dependencies */
 import styles from './GoodsList.module.scss';
 import Goods from 'components/MyDetailPage/Goods';
 import { getJjimListAPI } from 'api/jjimAPI';
-import mocHistoryList from 'api/historyAPI';
+import { getHistoryAPI } from 'api/userAPI';
 
 const cx = classNames.bind(styles);
 
-function sortByJjimTime(a, b) {
-  return a.jjim_date.getTime() > b.jjim_date.getTime()
-    ? -1
-    : a.jjim_date.getTime() < b.jjim_date.getTime()
-    ? 1
-    : 0;
-}
-
-function sortByHistoryTime(a, b) {
-  return a.history_date.getTime() > b.history_date.getTime()
-    ? -1
-    : a.history_date.getTime() < b.history_date.getTime()
-    ? 1
-    : 0;
-}
-
-
 function GoodsList(props) {
+  const [historyList, setHistoryList] = useState([]);
   const [jjimList, setJjimList] = useState([]);
-  useEffect(()=>{
-    getJjimListAPI().then(result =>{
+  useEffect(() => {
+    getJjimListAPI().then(result => {
       setJjimList(result.data);
     });
   }, []);
 
+  useEffect(() => {
+    getHistoryAPI().then(result => {
+      setHistoryList(result.data);
+    });
+  }, []);
+
   return (
-    <div className={cx('goods-list')} />
-      /*{ {props.show === 'history' &&
-        mocHistoryList.data
-          .sort(sortByHistoryTime)
-          .map(historyData => (
-            <Goods key={historyData.id} data={historyData} type={'history'} />
-          ))}
-      {props.show === 'jjim' &&
-        jjimList.map(jjimData => (
-            <Goods key={jjimData.id} data={jjimData} type={'jim'} />
-          ))
-          }*/
-    // </div>
+    <div className={cx('goods-list')}>
+      {historyList.map(history => (
+        <Goods data={history} type="history" />
+      ))}
+    </div>
   );
 }
 
