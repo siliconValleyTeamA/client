fda3195a1f0aab0eb499dc791d165b13908e1d3e
nohgijin <ynda11111@naver.com>
2021-01-16 21:48:35 +0900

    [#89] chore - project api 정리

--- src/api/projectAPI.js
+++ src/api/projectAPI.js
@@ -8,16 +8,16 @@ export const getSoonSuccessProjectListAPI = () => {
   return axios.get(`/api/project/success`);
 };
 
-export const getProjectDetailAPI = ({ projectId }) => {
-  return axios.get(`/api/project/${projectId}`);
-};
-
 export const getPopularProjectAPI = () => {
   return axios.get(`/api/project/popular`);
 };
 
 export const getCategoryProjectAPI = ({ categoryId, filterType }) => {
   return axios.get(`/api/project/category/${categoryId}/${filterType}`);
+};
+
+export const getProjectDetailAPI = ({ projectId }) => {
+  return axios.get(`/api/project/${projectId}`);
 };
 
 export const getProjectUserJjimAPI = ({ projectId }) => {
