39181284bea80bcafb7c37d1010448d0e3f9866a
nohgijin <ynda11111@naver.com>
2021-01-18 21:48:37 +0900

    [#97] feat - github 소셜 로그인

--- src/components/Mypage/LoginButton/LoginButton.js
+++ src/components/Mypage/LoginButton/LoginButton.js
@@ -5,15 +5,16 @@ import { AiFillGithub } from 'react-icons/ai';
 
 /* Internal dependencies */
 import styles from './LoginButton.module.scss';
-
 const cx = classNames.bind(styles);
 
 function LoginButton() {
   return (
-    <button className={cx('login')}>
-      <AiFillGithub className={cx('AiFillGithub')}/>
-      GitHub으로 로그인
-    </button>
+    <a href={'http://localhost:8080/auth/github/'}>
+      <button className={cx('login')}>
+        <AiFillGithub className={cx('AiFillGithub')} />
+        GitHub으로 로그인
+      </button>
+    </a>
   );
 }
 
