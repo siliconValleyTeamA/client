20890e8976b178a8722dba60ca900eb113b03d7d
OMEGA-Y <science_co@naver.com>
2021-01-23 23:22:52 +0900

    [#122] feat - 프로젝트 등록정보 컴포넌트로 분리 및 style 수정

---
+++ src/components/global/ProjectInfo/ProjectInfo.js
@@ -0,0 +1,57 @@
+/* External dependencies */
+import React, { useState } from 'react';
+import classNames from 'classnames/bind';
+
+/* Internal dependencies */
+import styles from './ProjectInfo.module.scss';
+const cx = classNames.bind(styles);
+
+function ProjectInfo({ value, func }) {
+  return (
+    <div className={cx('discription')}>
+      <div className={cx('leftside')}>
+        <label>프로젝트명</label>
+        <label>기업명</label>
+        <label>목표금액</label>
+        <label>시작일</label>
+        <label>종료일</label>
+        <label>카테고리</label>
+      </div>
+      <div className={cx('rightside')}>
+        <input
+          type="text"
+          onChange={func.onTitleChange}
+          value={value.titleValue}
+        />
+        <input
+          type="text"
+          onChange={func.onCompanyTitleValue}
+          value={value.companyTitleValue}
+        />
+        <input
+          type="number"
+          onChange={func.onGoalPriceValue}
+          value={value.goalPriceValue}
+        />
+        <input
+          type="date"
+          onChange={func.onStartDate}
+          value={value.startDate}
+        />
+        <input type="date" onChange={func.onEndDate} value={value.endDate} />
+        <select
+          onChange={func.onCategorySelectChange}
+          value={value.categoryValue}
+        >
+          {value.Category.map(item => (
+            <option key={item.key} value={item.key}>
+              {item.value}{' '}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectInfo;
