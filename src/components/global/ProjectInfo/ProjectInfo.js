87a4a9af0448b1ed30aea721a3c1616787b0586c
OMEGA-Y <science_co@naver.com>
2021-01-25 16:03:29 +0900

    [#125] chore - db에 이미지 여러장 저장

--- src/components/global/ProjectInfo/ProjectInfo.js
+++ src/components/global/ProjectInfo/ProjectInfo.js
@@ -1,17 +1,22 @@
 /* External dependencies */
-import React from 'react';
+import React, { useState } from 'react';
+import Dropzone from 'react-dropzone';
+import { AiOutlinePlusSquare } from 'react-icons/ai';
 import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
+import axios from 'api/axios';
 
 /* Internal dependencies */
 import styles from './ProjectInfo.module.scss';
-import FileUpload from 'components/AddProjectPage/Fileupload';
 import { createPorjectInfoAPI } from 'api/projectAPI';
 
 const cx = classNames.bind(styles);
 
 function ProjectInfo({ Category, info, func }) {
+  const [images, setImages] = useState([]);
+
   const createProjectInfo = () => {
+    info.images = images;
     createPorjectInfoAPI(info).then(result => {
       if (result.data.success) {
         alert('상품을 성공적으로 업로드하였습니다.');
@@ -21,17 +26,53 @@ function ProjectInfo({ Category, info, func }) {
     });
   };
 
+  const onDrop = files => {
+    let formData = new FormData();
+    const config = {
+      header: { 'content-type': 'multipart/form-data' },
+    };
+    formData.append('file', files[0]);
+
+    //save the Image we chose inside the Node Server
+    axios.post(`/api/project/uploadimage`, formData, config).then(result => {
+      setImages([...images, { filename: result.data.image }]);
+    });
+  };
+
   return (
     <div>
-      <FileUpload refreshFunction={func.onImages} />
+      <div className={cx('uploaddiv')}>
+        <Dropzone onDrop={onDrop} multiple={false} maxSize={800000000}>
+          {({ getRootProps, getInputProps }) => (
+            <div className={cx('picturediv')} {...getRootProps()}>
+              <input {...getInputProps()} />
+              <AiOutlinePlusSquare className={cx('icon')}></AiOutlinePlusSquare>
+              {images.map((image, index) => (
+                <div onClick={() => []}>
+                  <img
+                    style={{
+                      minWidth: '300px',
+                      width: '300px',
+                      height: '240px',
+                    }}
+                    src={`${image.filename}`}
+                    alt={`productImg-${index}`}
+                  />
+                </div>
+              ))}
+            </div>
+          )}
+        </Dropzone>
+      </div>
+
       <div className={cx('discription')}>
         <div className={cx('leftside')}>
-          <label>프로젝트명</label>
-          <label>기업명</label>
-          <label>목표금액</label>
-          <label>시작일</label>
-          <label>종료일</label>
-          <label>카테고리</label>
+          <label>Project Name</label>
+          <label>Company</label>
+          <label>Goal Money</label>
+          <label>Start Date</label>
+          <label>End Date</label>
+          <label>Category</label>
         </div>
         <div className={cx('rightside')}>
           <input type="text" onChange={func.onTitle} value={info.title} />
@@ -57,7 +98,7 @@ function ProjectInfo({ Category, info, func }) {
         </div>
       </div>
       <Link to={`/`}>
-        <button onClick={createProjectInfo}>다음</button>
+        <button onClick={createProjectInfo}>Upload</button>
       </Link>
     </div>
   );
