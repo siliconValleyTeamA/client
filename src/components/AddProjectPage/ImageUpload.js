085713901798d009efe68d3999caf0de17d5990d
OMEGA-Y <science_co@naver.com>
2021-01-26 13:52:52 +0900

    [#129] refactor - 컴포넌트 이름 ImageUpload로 변경

--- src/components/AddProjectPage/ImageUpload.js
+++ src/components/AddProjectPage/ImageUpload.js
@@ -1,25 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Dropzone from 'react-dropzone';
 import { AiOutlinePlusSquare } from 'react-icons/ai';
 import classNames from 'classnames/bind';
+<<<<<<< Updated upstream:src/components/AddProjectPage/Fileupload/Fileupload.js
 import axios from '../../../api/axios';
+=======
+import axios from 'api/axios';
+>>>>>>> Stashed changes:src/components/AddProjectPage/ImageUpload.js
 
 /* Internal dependencies */
-import styles from './Fileupload.module.scss';
+import styles from './ImageUpload.module.scss';
 
 const cx = classNames.bind(styles);
 
-function FileUpload(props) {
-  const [Images, setImages] = useState([]);
+function ImageUpload(props) {
+  const [images, setImages] = useState([]);
+
   const onDrop = files => {
     let formData = new FormData();
     const config = {
       header: { 'content-type': 'multipart/form-data' },
     };
-
     formData.append('file', files[0]);
+
     //save the Image we chose inside the Node Server
     axios.post(`/api/project/uploadimage`, formData, config).then(result => {
+<<<<<<< Updated upstream:src/components/AddProjectPage/Fileupload/Fileupload.js
       console.log(result.data);
       if (result.data.success) {
         setImages([...Images, result.data.image]);
@@ -27,16 +33,21 @@ function FileUpload(props) {
       } else {
         alert('Failed to save the Image in Server');
       }
+=======
+      setImages([...images, { filename: result.data.image }]);
+      props.upload([...images, { filename: result.data.image }]);
+>>>>>>> Stashed changes:src/components/AddProjectPage/ImageUpload.js
     });
   };
 
-  const onDelete = image => {
+  /* const onDelete = image => {
     const currentIndex = Images.indexOf(image);
     let newImages = [...Images];
     newImages.splice(currentIndex, 1);
     setImages(newImages);
     props.refreshFunction(newImages);
-  };
+  }; */
+
   return (
     <div className={cx('uploaddiv')}>
       <Dropzone onDrop={onDrop} multiple={false} maxSize={800000000}>
@@ -44,11 +55,15 @@ function FileUpload(props) {
           <div className={cx('picturediv')} {...getRootProps()}>
             <input {...getInputProps()} />
             <AiOutlinePlusSquare className={cx('icon')}></AiOutlinePlusSquare>
-            {Images.map((image, index) => (
-              <div onClick={() => onDelete(image)}>
+            {images.map((image, index) => (
+              <div onClick={() => []}>
                 <img
-                  style={{ minWidth: '300px', width: '300px', height: '240px' }}
-                  src={`${image}`}
+                  style={{
+                    minWidth: '300px',
+                    width: '300px',
+                    height: '240px',
+                  }}
+                  src={`${image.filename}`}
                   alt={`productImg-${index}`}
                 />
               </div>
@@ -60,4 +75,4 @@ function FileUpload(props) {
   );
 }
 
-export default FileUpload;
+export default ImageUpload;
