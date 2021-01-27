c6d5d06143e4dbbcfcf949f3649c601c915f7d3a
OMEGA-Y <science_co@naver.com>
2021-01-27 17:53:47 +0900

    [#144] chore - En detail page 구조 정리

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 export { default as HomePage } from './HomePage';
 export { default as CategoryPage } from './CategoryPage';
-export { default as DetailPage } from './DetailPage';
+export { default as DetailPage } from './DetailPage/EnDetailPage';
 export { default as MyCartPage } from './MyCartPage';
 export { default as MyHistoryPage } from './MyHistoryPage';
 export { default as MyJjimPage } from './MyJjimPage';
