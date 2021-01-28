4c15a715a63340c47b174777f4b34062e8975bde
yeonjeonglee <yeonjeong.lee00@gmail.com>
2021-01-29 01:29:19 +0900

    bugfix - 장바구니 에러 수정

--- src/components/DetailPage/Drawer/Drawer.jsx
+++ src/components/DetailPage/Drawer/Drawer.jsx
@@ -11,7 +11,7 @@ const cx = classNames.bind(styles);
 
 function Drawer({ open, setOpen, data }) {
   const link = useLocation();
-  const projectId = link.pathname.split('/')[2];
+  const projectId = link.pathname.split('/')[3];
 
   const [money, setMoney] = useState('');
 
