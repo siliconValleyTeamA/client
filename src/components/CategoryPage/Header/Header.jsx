29ba8c724c5e6b388bab5f126ef2414a151caed7
유 창헌 <dbckdgjs1130@naver.com>
2021-02-03 16:25:46 +0900

    [#171] fix - 버그고침

--- src/components/CategoryPage/Header/Header.jsx
+++ src/components/CategoryPage/Header/Header.jsx
@@ -10,11 +10,10 @@ import styles from './Header.module.scss';
 const cx = classNames.bind(styles);
 
 function Header({ headerClick }) {
-  const category = useSelector(state => state.categoryReducer);
-
+  const category = useSelector(state => state.categoryReducer); 
   return (
     <div className={cx('wrapper')} onClick={headerClick}>
-      <div className={cx('header')}>{category.label}</div>
+      <div className={cx('header')}>{category.key}</div>
       <RiArrowDownSLine className={cx('RiArrowDownSLine')} />
     </div>
   );
