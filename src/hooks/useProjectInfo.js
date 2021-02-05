cae3811435eebb5cf198d381e1a734e25285bd8c
yeonjeonglee <yeonjeong.lee00@gmail.com>
2021-02-05 12:11:13 +0900

    chore - 카테고리 영어로 수정

--- src/hooks/useProjectInfo.js
+++ src/hooks/useProjectInfo.js
@@ -4,19 +4,19 @@ import { useState } from 'react';
 /* Internal dependencies */
 
 const Category = [
-  { key: 1, value: '패션-잡화' },
-  { key: 2, value: '뷰티' },
-  { key: 3, value: '푸드' },
-  { key: 4, value: '홈리빙' },
-  { key: 5, value: '디자인소품' },
-  { key: 6, value: '여행-레저' },
-  { key: 7, value: '스포츠-모빌리티' },
-  { key: 8, value: '반려동물' },
-  { key: 9, value: '모임' },
-  { key: 10, value: '공연-컬쳐' },
-  { key: 11, value: '소셜-캠페인' },
-  { key: 12, value: '교육-키즈' },
-  { key: 13, value: '게임-취미' },
+  { key: 1, value: 'fashion' },
+  { key: 2, value: 'beauty' },
+  { key: 3, value: 'food' },
+  { key: 4, value: 'homeliving' },
+  { key: 5, value: 'design' },
+  { key: 6, value: 'travel' },
+  { key: 7, value: 'sport' },
+  { key: 8, value: 'pet' },
+  { key: 9, value: 'meeting' },
+  { key: 10, value: 'culture' },
+  { key: 11, value: 'social' },
+  { key: 12, value: 'education' },
+  { key: 13, value: 'game' },
 ];
 
 function useProjectInfo() {
